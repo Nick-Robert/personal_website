85fb92da059448e63fb4969ff2c157157b129a4c
Nick Robert <nick.robert202@gmail.com>
2022-02-24 12:31:20 -0600

    One style active now

--- functions.js
+++ functions.js
@@ -10,24 +10,24 @@ function qualifications_button() {
     window.location.href="qualifications.html";
 }
 
-function style_toggle_button() {
-    var current_theme = document.getElementsByTagName('link')[0];
-    if (current_theme.getAttribute('href') == 'style_1.css') {
-        current_theme.setAttribute('href', 'style_2.css');
-        localStorage.setItem("style", "2");
-    } else {
-        current_theme.setAttribute('href', 'style_1.css');
-        localStorage.setItem("style", "1");
-    }
-}
+// function style_toggle_button() {
+//     var current_theme = document.getElementsByTagName('link')[0];
+//     if (current_theme.getAttribute('href') == 'style_1.css') {
+//         current_theme.setAttribute('href', 'style_2.css');
+//         localStorage.setItem("style", "2");
+//     } else {
+//         current_theme.setAttribute('href', 'style_1.css');
+//         localStorage.setItem("style", "1");
+//     }
+// }
 
-window.onload = function() {
-    var current_theme = document.getElementsByTagName('link')[0];
-    if (localStorage.getItem("style") === "2") {
-        current_theme.setAttribute('href', 'style_2.css');
-    } else if (localStorage.getItem("style") === "2") {
-        current_theme.setAttribute('href', 'style_1.css');
-    } else {
-        current_theme.setAttribute('href', 'style_1.css');
-    }
-}
+// window.onload = function() {
+//     var current_theme = document.getElementsByTagName('link')[0];
+//     if (localStorage.getItem("style") === "2") {
+//         current_theme.setAttribute('href', 'style_2.css');
+//     } else if (localStorage.getItem("style") === "2") {
+//         current_theme.setAttribute('href', 'style_1.css');
+//     } else {
+//         current_theme.setAttribute('href', 'style_1.css');
+//     }
+// }
