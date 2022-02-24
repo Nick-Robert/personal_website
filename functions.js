24858013cfa4994897e4baa1f9e4521e3c066cbb
Nick <77978140+Nick-Robert@users.noreply.github.com>
2022-02-24 12:14:33 -0600

    Starter
    Starter code comes from my submission for my programming studio class.

---
+++ functions.js
@@ -0,0 +1,33 @@
+function home_button() {
+    window.location.href="index.html";
+}
+
+function tech_portfolio_button() {
+    window.location.href = "tech-portfolio.html";
+}
+
+function qualifications_button() {
+    window.location.href="qualifications.html";
+}
+
+function style_toggle_button() {
+    var current_theme = document.getElementsByTagName('link')[0];
+    if (current_theme.getAttribute('href') == 'style_1.css') {
+        current_theme.setAttribute('href', 'style_2.css');
+        localStorage.setItem("style", "2");
+    } else {
+        current_theme.setAttribute('href', 'style_1.css');
+        localStorage.setItem("style", "1");
+    }
+}
+
+window.onload = function() {
+    var current_theme = document.getElementsByTagName('link')[0];
+    if (localStorage.getItem("style") === "2") {
+        current_theme.setAttribute('href', 'style_2.css');
+    } else if (localStorage.getItem("style") === "2") {
+        current_theme.setAttribute('href', 'style_1.css');
+    } else {
+        current_theme.setAttribute('href', 'style_1.css');
+    }
+}
